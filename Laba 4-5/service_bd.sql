64b44c9c734c006ee3d9dae0eabf4e4aa8306640
Et7s <124525048+Et7s@users.noreply.github.com>
2023-05-19 15:57:06 +0300

    Update service_bd.sql

--- Laba 4-5/service_bd.sql
+++ Laba 4-5/service_bd.sql
@@ -1,13 +1,12 @@
-CREATE SCHEMA service;
-CREATE TABLE service.users ( id serial PRIMARY KEY, full_name varchar (55) NOT NULL, login varchar (120) NOT NULL, password varchar (120) NOT NULL);
+CREATE TABLE LABA9 ( id serial PRIMARY KEY, users varchar (55) NOT NULL, login varchar (120) NOT NULL, password varchar (120) NOT NULL);
 
-INSERT INTO service.users (id, full_name, login, password) VALUES (1, 'Ivanov Ivan', 'ivanov', '123456'); 
-INSERT INTO service.users (id, full_name, login, password) VALUES (2, 'Tina Sanders', 'TiSa', '123456');
-INSERT INTO service.users (id, full_name, login, password) VALUES (3, 'Steven Brown', 'StBr', '111111'); 
-INSERT INTO service.users (id, full_name, login, password) VALUES (4, 'Charles Stone', 'ChSt', '222222');
-INSERT INTO service.users (id, full_name, login, password) VALUES (5, 'Deborah Barker', 'DeBa', '333333'); 
-INSERT INTO service.users (id, full_name, login, password) VALUES (6, 'Ruby Allen', 'RuAl', '555555');
-INSERT INTO service.users (id, full_name, login, password) VALUES (7, 'Margaret Carroll', 'MaCa', '121212'); 
-INSERT INTO service.users (id, full_name, login, password) VALUES (8, 'Christopher Arnold', 'ChAr', '323232');
-INSERT INTO service.users (id, full_name, login, password) VALUES (9, 'Martha Andrews', 'MaAn', '010101'); 
-INSERT INTO service.users (id, full_name, login, password) VALUES (10, 'Shannon Evans', 'ShEv', '999999');
+INSERT INTO service.users (id, users, login, password) VALUES (1, 'Ivanov Ivan', 'ivanov', '123456'); 
+INSERT INTO service.users (id, users, login, password) VALUES (2, 'Tina Sanders', 'TiSa', '123456');
+INSERT INTO service.users (id, users, login, password) VALUES (3, 'Steven Brown', 'StBr', '111111'); 
+INSERT INTO service.users (id, users, login, password) VALUES (4, 'Charles Stone', 'ChSt', '222222');
+INSERT INTO service.users (id, users, login, password) VALUES (5, 'Deborah Barker', 'DeBa', '333333'); 
+INSERT INTO service.users (id, users, login, password) VALUES (6, 'Ruby Allen', 'RuAl', '555555');
+INSERT INTO service.users (id, users, login, password) VALUES (7, 'Margaret Carroll', 'MaCa', '121212'); 
+INSERT INTO service.users (id, users, login, password) VALUES (8, 'Christopher Arnold', 'ChAr', '323232');
+INSERT INTO service.users (id, users, login, password) VALUES (9, 'Martha Andrews', 'MaAn', '010101'); 
+INSERT INTO service.users (id, users, login, password) VALUES (10, 'Shannon Evans', 'ShEv', '999999');
